0ce2aa82a2ea796c087f11595c5403e9d118af7f
Peter Cassell <pc0122@my.bristol.ac.uk>
2020-06-03 18:16:00 +0100

    first

---
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,78 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.HelloButton = new System.Windows.Forms.Button();
+            this.HelloText = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // HelloButton
+            // 
+            this.HelloButton.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.HelloButton.Location = new System.Drawing.Point(42, 295);
+            this.HelloButton.Name = "HelloButton";
+            this.HelloButton.Size = new System.Drawing.Size(186, 118);
+            this.HelloButton.TabIndex = 0;
+            this.HelloButton.Text = "button1";
+            this.HelloButton.UseVisualStyleBackColor = true;
+            this.HelloButton.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // HelloText
+            // 
+            this.HelloText.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.HelloText.Location = new System.Drawing.Point(42, 90);
+            this.HelloText.Name = "HelloText";
+            this.HelloText.Size = new System.Drawing.Size(371, 34);
+            this.HelloText.TabIndex = 1;
+            this.HelloText.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.HelloText);
+            this.Controls.Add(this.HelloButton);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button HelloButton;
+        private System.Windows.Forms.TextBox HelloText;
+    }
+}
+
